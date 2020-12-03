12a56e7fb12098bf837c7da4dc104cbe84453172
DiogoIspg <57219615+DiogoIspg@users.noreply.github.com>
2020-12-03 18:12:16 +0000

    Solvers for Day 2 and Day 3 2020

---
+++ AdventOfCode/Day_02.cs
@@ -0,0 +1,54 @@
+﻿using AoCHelper;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public class Day_02 : BaseDay
+    {
+        private readonly string _input;
+
+        public Day_02()
+        {
+            _input = File.ReadAllText(InputFilePath);
+        }
+
+        public override string Solve_1()
+        {
+            var lines = _input.Split('\n');
+            var count = 0;
+
+            for (int i = 0; i < lines.Count(); i++)
+            {
+                var part = lines[i].Split(' ');
+
+                if (part[2].Count(x => x == part[1][0]) >= int.Parse(part[0].Split("-")[0])
+                    && part[2].Count(x => x == part[1][0]) <= int.Parse(part[0].Split("-")[1]))
+                {
+                    count++;
+                }
+            }
+
+            return count.ToString();
+        }
+
+        public override string Solve_2()
+        {
+            var lines = _input.Split('\n');
+            var count = 0;
+
+            for (int i = 0; i < lines.Count(); i++)
+            {
+                var part = lines[i].Split(' ');
+
+                if (part[2][int.Parse(part[0].Split('-')[0]) - 1] == part[1][0]
+                    ^ part[2][int.Parse(part[0].Split('-')[1]) - 1] == part[1][0])
+                {
+                    count++;
+                }
+            }
+
+            return count.ToString();
+        }
+    }
+}
---
+++ AdventOfCode/Day_03.cs
@@ -0,0 +1,50 @@
+﻿using AoCHelper;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public class Day_03 : BaseDay
+    {
+        private readonly string _input;
+        private readonly string[] _lines;
+
+        public Day_03()
+        {
+            _input = File.ReadAllText(InputFilePath);
+            _lines = _input.Split('\n').Select(x => x.Trim()).ToArray();
+        }
+
+        public override string Solve_1() => Solve_1(3, 1).ToString();
+
+        public long Solve_1(int right, int down)
+        {
+            var pos = (x: 0, y: 0);
+
+            long result = 0;
+
+            while (_lines.Count() > pos.y)
+            {
+                if (_lines[pos.y][pos.x] == '#')
+                {
+                    result++;
+                }
+
+                pos.x += right;
+                pos.y += down;
+
+                if (pos.x >= _lines[pos.y - down].Count())
+                {
+                    pos.x -= _lines[pos.y - down].Count();
+                }
+            }
+
+            return result;
+        }
+
+        public override string Solve_2()
+        {
+            return (Solve_1(1, 1) * Solve_1(3, 1) * Solve_1(5, 1) * Solve_1(7, 1) * Solve_1(1, 2)).ToString();
+        }
+    }
+}
