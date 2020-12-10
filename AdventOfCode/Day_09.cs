5161b82c4927b1897574d3c2c232220903314ec1
DiogoIspg <57219615+DiogoIspg@users.noreply.github.com>
2020-12-10 17:49:37 +0000

    Day 9 and 10 solutions

---
+++ AdventOfCode/Day_09.cs
@@ -0,0 +1,100 @@
+﻿using AoCHelper;
+using System.IO;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AdventOfCode
+{
+    public class Day_09 : BaseDay
+    {
+        private readonly string _input;
+        private readonly string[] _lines;
+
+        public Day_09()
+        {
+            _input = File.ReadAllText(InputFilePath);
+            _lines = File.ReadAllLines(InputFilePath);
+        }
+
+        public override string Solve_1()
+        {
+            return "";
+
+            var current = 25;
+            IOrderedEnumerable<UInt64> ordered25;
+            UInt64 n;
+            do
+            {
+                var next25 = _lines.Skip(current - 25).Take(25);
+                ordered25 = next25.Select(UInt64.Parse).OrderBy(x => x);
+                n = UInt64.Parse(_lines.ElementAt(current));
+                ++current;
+            } while (IsCorrectNumber(n, ordered25));
+
+            return n.ToString();
+        }
+
+        public bool IsCorrectNumber(UInt64 n, IOrderedEnumerable<UInt64> last25)
+        {
+            int low = 0;
+            int high = last25.Count() - 1;
+
+            while (low != high)
+            {
+                var sum = last25.ElementAt(low) + last25.ElementAt(high);
+                if (sum == n)
+                    return true;
+
+                if (sum > n)
+                {
+                    --high;
+                }
+                if (sum < n)
+                {
+                    ++low;
+                }
+            }
+
+            return false;
+        }
+
+        public override string Solve_2()
+        {
+            UInt64 resFromPart1 = 258585477;
+
+            int seq = 2;
+            int current = 0;
+            var longLines = _lines.Select(x => UInt64.Parse(x)).ToList();
+
+            do
+            {
+                var numbers = Enumerable.Range(current, seq).Select(i => longLines.ElementAt(i));
+
+                UInt64 sum = 0;
+
+                for (int i = 0; i < numbers.Count(); i++)
+                {
+                    sum += numbers.ElementAt(i);
+                }
+
+                if (sum == resFromPart1)
+                {
+                    return (numbers.Min() + numbers.Max()).ToString();
+                }
+                else if (sum > resFromPart1)
+                {
+                    ++current;
+                    seq = 2;
+                }
+                else
+                {
+                    ++seq;
+                }
+            } while (current - seq != longLines.Count);
+
+            return "Not found!";
+        }
+    }
+}
---
+++ AdventOfCode/Day_10.cs
@@ -0,0 +1,83 @@
+﻿using AoCHelper;
+using System.IO;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AdventOfCode
+{
+    public class Day_10 : BaseDay
+    {
+        private readonly string _input;
+        private readonly string[] _lines;
+
+        public Day_10()
+        {
+            _input = File.ReadAllText(InputFilePath);
+            _lines = File.ReadAllLines(InputFilePath);
+        }
+
+        public override string Solve_1()
+        {
+            var adapters = _lines.Select(i => int.Parse(i)).OrderBy(i => i).ToList();
+            adapters.Insert(0, 0);
+            adapters.Add(adapters.ElementAt(adapters.Count - 1) + 3);
+
+            var countDiffOne = 0;
+            var countDiffThree = 0;
+
+            for (int i = 1; i < adapters.Count; i++)
+            {
+                var diff = adapters.ElementAt(i) - adapters.ElementAt(i - 1);
+                if (diff == 1)
+                {
+                    ++countDiffOne;
+                }
+                else if (diff == 3)
+                {
+                    ++countDiffThree;
+                }
+            }
+
+            return (countDiffOne * countDiffThree).ToString();
+        }
+
+        public override string Solve_2()
+        {
+            var adapters = _lines.Select(i => int.Parse(i)).OrderBy(i => i).ToList();
+            adapters.Insert(0, 0);
+            adapters.Add(adapters.ElementAt(adapters.Count - 1) + 3);
+
+            var dict = new Dictionary<int, ulong>();
+            dict.Add(adapters.First(), 1);
+
+            for (int i = 0; i < adapters.Count; i++)
+            {
+                var j = i + 1;
+
+                if (j > adapters.Count - 1)
+                    break;
+
+                while (adapters.ElementAt(j) - adapters.ElementAt(i) <= 3)
+                {
+                    if (dict.ContainsKey(adapters.ElementAt(j)))
+                    {
+                        dict[adapters.ElementAt(j)] += dict[adapters.ElementAt(i)];
+                    }
+                    else
+                    {
+                        dict.Add(adapters.ElementAt(j), dict[adapters.ElementAt(i)]);
+                    }
+
+                    ++j;
+
+                    if (j > adapters.Count - 1)
+                        break;
+                }
+            }
+
+            return dict[adapters.ElementAt(adapters.Count - 1)].ToString();
+        }
+    }
+}
